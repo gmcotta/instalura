55faa0cca8589cc7c759e11c0d9737d206d695b1
Gustavo Cotta <gmcotta34@gmail.com>
2021-02-13 17:43:26 -0300

    Linting files

--- pages/index.js
+++ pages/index.js
@@ -28,42 +28,44 @@ export default function Home() {
         }}
       >
         <Grid.Row>
-          <Grid.Col 
-            offset={{ xs: 0, md: 1}} 
+          <Grid.Col
+            offset={{ xs: 0, md: 1 }}
             value={{ xs: 12, md: 5 }}
             display="flex"
             flexDirection="column"
             alignItems="flex-start"
             justifyContent="center"
           >
-            <Text 
-              variant="title" 
-              tag="h1" 
-              color="tertiary.main" 
+            <Text
+              variant="title"
+              tag="h1"
+              color="tertiary.main"
               textAlign={{
-                xs: "center",
-                md: "left"
+                xs: 'center',
+                md: 'left',
               }}
             >
               Compartilhe momentos e conecte-se com amigos
             </Text>
-            <Text 
-              variant="paragraph1" 
-              tag="p" 
+            <Text
+              variant="paragraph1"
+              tag="p"
               color="tertiary.light"
               textAlign={{
-                xs: "center",
-                md: "left"
+                xs: 'center',
+                md: 'left',
               }}
             >
-              Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, nisi eros vermeio. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit.
+              Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum
+              girarzis, nisi eros vermeio. Posuere libero varius. Nullam a nisl
+              ut ante blandit hendrerit.
             </Text>
-            <Button 
+            <Button
               variant="primary.main"
               display="block"
               margin={{
-                xs: "auto",
-                md: "initial"
+                xs: 'auto',
+                md: 'initial',
               }}
             >
               Cadastrar
@@ -71,6 +73,7 @@ export default function Home() {
           </Grid.Col>
           <Grid.Col value={{ xs: 12, md: 6 }}>
             <img
+              alt="Imagem de dois celulares mostrando o perfil do Nicolas Cage"
               style={{ display: 'block', margin: 'auto' }}
               src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
             />
