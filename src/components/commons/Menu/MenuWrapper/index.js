7aa810f981d1299fb11b1c6e1d96e8e1d83eb59d
Gustavo Cotta <gmcotta34@gmail.com>
2021-02-12 21:21:22 -0300

    Changing Menu style file to MenuWrapper component

--- src/components/commons/Menu/MenuWrapper/index.js
+++ src/components/commons/Menu/MenuWrapper/index.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
-import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
-import { TextStypeVariantsMap } from '../../foundation/Text';
+import { breakpointsMedia } from '../../../../theme/utils/breakpointsMedia';
+import { TextStypeVariantsMap } from '../../../foundation/Text';
 
 export const MenuWrapper = styled.nav`
   font-family: 'Rubik', sans-serif;
--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Logo from '../../../theme/Logo';
 import Text from '../../foundation/Text';
 import { Button } from '../Button';
-import {MenuWrapper} from './styles';
+import {MenuWrapper} from './MenuWrapper';
 
 export default function Menu() {
   const links = [
