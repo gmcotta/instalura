4bed1647211e881e2a043f035b8cb46a355cb41b
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-13 16:56:13 -0300

    feat(website page wrapper): create website page wrapper component and use it on home

---
+++ src/components/wrappers/WebsitePage/index.js
@@ -0,0 +1,52 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import Footer from '../../commons/Footer';
+import Menu from '../../commons/Menu';
+import Modal from '../../commons/Modal';
+import Box from '../../foundation/layout/Box';
+import FormCadastro from '../../patterns/FormCadastro';
+
+export const WebsitePageContext = createContext({
+  openModalCadastrar: () => {},
+  closeModalCadastrar: () => {},
+});
+
+export default function WebsitePageWrapper({ children }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+  return (
+    <WebsitePageContext.Provider value={{
+      openModalCadastrar: () => { openModal(); },
+      closeModalCadastrar: () => { closeModal(); },
+    }}
+    >
+      <Box
+        display="flex"
+        flex="1"
+        flexDirection="column"
+      >
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          {(modalProps) => (
+            <FormCadastro modalProps={modalProps} onClose={closeModal} />
+          )}
+        </Modal>
+        <Menu onCadastrarClick={openModal} />
+        {children}
+        <Footer />
+      </Box>
+    </WebsitePageContext.Provider>
+  );
+}
+
+WebsitePageWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
