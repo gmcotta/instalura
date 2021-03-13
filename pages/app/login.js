af2b3f04ac3f1d50f2a9c1ba3b347c7562c97113
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-12 22:28:39 -0300

    feat(login page): create login page

---
+++ pages/app/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div>
+      Login
+      <a href="/">Voltar para home recarregando a página</a>
+    </div>
+  );
+}
