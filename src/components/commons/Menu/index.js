c21dc986b0807fc05966b2dbe3e415a8646de29a
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-12 22:59:45 -0300

    feat(menu): add link on login button

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -44,7 +44,7 @@ export default function Menu({ onCadastrarClick }) {
         }
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-        <Button ghost variant="secondary.main">Entrar</Button>
+        <Button ghost variant="secondary.main" href="/app/login">Entrar</Button>
         <Button variant="primary.main" onClick={onCadastrarClick}>Cadastrar</Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
