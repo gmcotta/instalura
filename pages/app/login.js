d1ee0c73975c1e9816a290160b6d3d8e893ecb89
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-23 21:55:53 -0300

    refactor(login): create login form component

--- pages/app/login.js
+++ pages/app/login.js
@@ -1,41 +1,13 @@
 import React from 'react';
-import Button from '../../src/components/commons/Button';
+
 import Link from '../../src/components/commons/Link';
-import TextField from '../../src/components/forms/TextField';
 import Box from '../../src/components/foundation/layout/Box';
 import Grid from '../../src/components/foundation/layout/Grid';
 import Text from '../../src/components/foundation/Text';
 import { WebsitePageContext } from '../../src/components/wrappers/WebsitePage';
 import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 import Logo from '../../src/theme/Logo';
-
-function LoginForm() {
-  return (
-    <form id="formCadastro" action="/app/profile">
-      <TextField
-        placeholder="Usuário"
-        name="usuario"
-      />
-      <TextField
-        placeholder="Senha"
-        name="senha"
-        type="password"
-      />
-
-      <Button
-        type="submit"
-        variant="primary.main"
-        margin={{
-          xs: '0 auto',
-          md: 'initial',
-        }}
-        fullWidth
-      >
-        Entrar
-      </Button>
-    </form>
-  );
-}
+import FormLogin from '../../src/components/patterns/FormLogin';
 
 function LoginScreen() {
   const websitePageContext = React.useContext(WebsitePageContext);
@@ -73,7 +45,7 @@ function LoginScreen() {
               <Logo size="large" />
             </Link>
           </Box>
-          <LoginForm />
+          <FormLogin />
           <Text
             variant="paragraph1"
             tag="p"
