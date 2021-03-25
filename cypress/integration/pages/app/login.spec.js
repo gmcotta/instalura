3230d36e1f53d0b711f41aea43ba143ddc7f1587
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-25 18:34:10 -0300

    test(login): create test for store jwt

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -4,10 +4,28 @@ describe('/pages/app/login', () => {
   it('should go to /app/profile', () => {
     cy.visit('/app/login/');
 
-    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
+    cy.get('#formCadastro input[name="username"]').type('omariosouto');
+    cy.get('#formCadastro input[name="password"]').type('senhasegura');
     cy.get('#formCadastro button[type="submit"]').click();
 
     cy.url().should('include', '/app/profile');
+  });
+
+  it('should store jwt token', () => {
+    cy.intercept('https://instalura-api.omariosouto.vercel.app/api/login')
+      .as('userLogin');
+    cy.visit('/app/login/');
+
+    cy.get('#formCadastro input[name="username"]').type('omariosouto');
+    cy.get('#formCadastro input[name="password"]').type('senhasegura');
+    cy.get('#formCadastro button[type="submit"]').click();
+
+    cy.wait('@userToken').then((intercept) => {
+      const { token } = intercept.response.body.data;
+
+      cy.getCookie('APP_TOKEN')
+        .should('exist')
+        .should('have.property', 'value', token);
+    });
   });
 });
