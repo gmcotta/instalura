05953e44e1650e3a8fac88e9c993148476daf17f
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-26 21:08:53 -0300

    chore: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
-import 'jest-styled-components';
-import 'jest-canvas-mock';
-import '@testing-library/jest-dom';
-
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.config.js'],
   moduleDirectories: [
